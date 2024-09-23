3baf922b0efd9f7463345ebbbcde5cd7431a3e71
angiemusu <149411897+angiemusu@users.noreply.github.com>
2024-09-22 19:07:06 -0500

    Repositories

---
+++ TerraPlan/src/main/java/pe/edu/upc/terraplan/repositories/ITerrenoRepository.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.si62_grupo_01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.si62_grupo_01.entities.Terreno;
+
+import java.util.List;
+
+@Repository
+public interface ITerrenoRepository extends JpaRepository<Terreno,Integer> {
+    @Query(value ="SELECT t.ubicacion_terreno, COUNT(*) AS num_terrenos\n" +
+            "FROM terreno t\n" +
+            "GROUP BY t.ubicacion_terreno;" ,nativeQuery = true)
+    public List<String[]>cantidadTerrenosUbicacion();
+}
